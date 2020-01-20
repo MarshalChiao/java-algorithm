5ffd964f98b320c7da10448315ae85b1bfb061c2
shaoshuai_jiao <shaoshuai_jiao@kingdee.com>
2020-01-20 15:16:49 +0800

    add insertion sort

--- src/main/java/com/marshal/sort/InsertionSort.java
+++ src/main/java/com/marshal/sort/InsertionSort.java
@@ -7,9 +7,30 @@ package com.marshal.sort;
  * @Description:
  */
 
+import java.util.Arrays;
+
 /**
  * 插入排序
  * 对数组 [2,1,7,9,5,8] 从小到大的顺序进行插入排序
  */
 public class InsertionSort {
+
+    public void sort(int[] nums) {
+        for (int i = 1, j, current; i < nums.length; i++) {
+            current = nums[i];
+
+            for (j = i - 1; j >= 0 && nums[j] > current; j--) {
+                nums[j + 1] = nums[j];
+            }
+
+            nums[j + 1] = current;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,7,9,5,8};
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 }
